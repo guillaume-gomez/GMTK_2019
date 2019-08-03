3f19681497b1204b5310221e8eb16e8b22582147
Moustafa Koterba <mou>
2019-08-03 23:15:19 +0200

    disabled loop on platform rotation

--- Assets/Scripts/Gameplay/MovingPlatform.cs
+++ Assets/Scripts/Gameplay/MovingPlatform.cs
@@ -22,25 +22,26 @@ public class MovingPlatform : MonoBehaviour
     [Header("Parameters")]
     [SerializeField] private MoveType moveType = MoveType.TRANSLATION;
 
-    [Header("Rotation parameters")]
-    [SerializeField] [Range(0,0.5f)] private float translationSpeed = 0.1f;
-
     [Header("Translation parameters")]
+    [SerializeField] private bool loop;
+    [SerializeField] [Range(0, 0.5f)] private float translationSpeed = 0.1f;
+
+    [Header("Rotation parameters")]
     [SerializeField] private RotationDirection rotationDirection = RotationDirection.CLOCKWISE;
     [SerializeField] private float rotationSpeed = 2f;
     [SerializeField] private float rotationRadius = 2.5f;
 
-    private Vector3 m_startPosition = Vector3.zero;
+    private Vector3 m_startPosition = Vector3.one;
+    private Vector3 m_rotationVector = Vector3.zero;
     private bool m_hasReachedPointer = false;
     private bool m_hasReachedStartPos = false;
 
-    private float m_posX;
-    private float m_posY;
     private float m_angle = 0f;
+    private float m_posX = 0f;
+    private float m_posY = 0f;
 
     void Start()
     {
-        m_startPosition = transform.position;
     }
 
     private void Update()
@@ -53,11 +54,12 @@ public class MovingPlatform : MonoBehaviour
             return;
         }
 
+
         if (moveType == MoveType.TRANSLATION)
         {
             if (!m_hasReachedPointer)
                 this.transform.position = Vector3.MoveTowards(this.transform.position, pointerReference.position, translationSpeed);
-            else
+            else if (loop)
                 this.transform.position = Vector3.MoveTowards(this.transform.position, m_startPosition, translationSpeed);
 
             if (Vector3.Distance(transform.position, pointerReference.position) < 0.001f)
@@ -82,9 +84,6 @@ public class MovingPlatform : MonoBehaviour
             m_posY = pointerReference.position.y + Mathf.Sin(m_angle) * rotationRadius;
             transform.position = new Vector3(m_posX, m_posY, transform.position.z);
             m_angle = m_angle + Time.deltaTime * rotationSpeed * (rotationDirection == RotationDirection.COUNTER_CLOCKWISE ? -1 : 1);
-
-            if (m_angle >= 360f)
-                m_angle = 0f;
         }
     }
 }
