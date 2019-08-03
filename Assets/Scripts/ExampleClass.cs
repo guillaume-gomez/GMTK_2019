65fe06a9cdbce1e4b0d5cb8e14a5f9009da7b07b
Godard Ryad <ryadgodard@gmail.com>
2019-08-03 04:39:10 +0200

    Test level 1 for Guillaume

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExampleClass : MonoBehaviour
+{
+    public float thrust = 15f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.right * thrust);
+    }
+}
