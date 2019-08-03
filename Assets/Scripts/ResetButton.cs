2d2b1706ef59778791ce25b64a3a31922b66a6e1
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-08-03 17:02:20 +0200

    wip button

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetButton : MonoBehaviour
+{
+  public void OnClick()
+  {
+    Debug.Log("buton cliked");
+  }
+}
