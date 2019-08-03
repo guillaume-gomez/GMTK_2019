3ff5d634b6ffc3fdc42e56e18b3ca758956bb3c1
Moustafa Koterba <mou>
2019-08-03 14:42:59 +0200

    fix trampoline (player can not jump on other side)

--- Assets/Scripts/Gameplay/Trampoline.cs
+++ Assets/Scripts/Gameplay/Trampoline.cs
@@ -9,8 +9,18 @@ public class Trampoline : MonoBehaviour
     {
         if (collision.collider == null) return;
         if (collision.collider.attachedRigidbody == null) return;
+        Debug.Log("on collision enter");
+        //var force = Vector3.up * heightForce - collision.collider.attachedRigidbody.velocity;
+        //collision.collider.attachedRigidbody.AddForce(force, ForceMode.Impulse);   
+    }
 
-        var force = Vector3.up * heightForce - collision.collider.attachedRigidbody.velocity;
-        collision.collider.attachedRigidbody.AddForce(force, ForceMode.Impulse);   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == null) return;
+        if (other.attachedRigidbody == null) return;
+        Debug.Log("on trigger enter");
+
+        var force = Vector3.up * heightForce - other.attachedRigidbody.velocity;
+        other.attachedRigidbody.AddForce(force, ForceMode.Impulse);
     }
 }
