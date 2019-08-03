72f94d2f3a58712407031cfeee8d1c240dfce898
Moustafa Koterba <mou>
2019-08-03 14:00:43 +0200

    trampoline added

---
+++ Assets/Scripts/Gameplay/Trampoline.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] private float heightForce = 0f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == null) return;
+        if (collision.collider.attachedRigidbody == null) return;
+
+        Debug.Log("on collision");
+        var force = Vector3.up * heightForce - collision.collider.attachedRigidbody.velocity;
+        Debug.Log(force);
+        collision.collider.attachedRigidbody.AddForce(force, ForceMode.Impulse);   
+    }
+}
