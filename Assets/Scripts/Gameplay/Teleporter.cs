0d0198d1fc434de262670ae8360b570b1002349f
Moustafa Koterba <mou>
2019-08-03 18:18:44 +0200

    working version of teleporter

--- Assets/Scripts/Gameplay/Teleporter.cs
+++ Assets/Scripts/Gameplay/Teleporter.cs
@@ -10,13 +10,49 @@ public class Teleporter : MonoBehaviour
     [Header("Modular Variables")]
     [SerializeField] private VectorVariable teleporterPosition;
     [SerializeField] private VectorVariable otherTeleporterPosition;
+    [SerializeField] private BoolVariable hasExitTeleporter;
+    [SerializeField] private BoolVariable hasExitOtherTeleporter;
 
     private Collider m_currentCollider;
 
+    private void Awake()
+    {
+        if (!hasExitTeleporter)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            Debug.LogError("hasExitOtherTeleporter not set, can not set value");
+#endif
+            return;
+        }
+
+        hasExitTeleporter.Value = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(name + " on trigger enter");
+
+        if (!hasExitOtherTeleporter)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            Debug.LogError("hasExitOtherTeleporter not set, can not check teleporter");
+#endif
+            return;
+        }
+
+        if (hasExitOtherTeleporter.Value) return;
+        //if (!hasExitTeleporter.Value) return;
+
         if (other == null) return;
         if (other.attachedRigidbody == null) return;
+
+        if (!hasExitTeleporter)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            Debug.LogError("hasExitOtherTeleporter not set, can not check teleporter");
+#endif
+            return;
+        }
 
         if (!onTeleporterCollision)
         {
@@ -26,9 +62,6 @@ public class Teleporter : MonoBehaviour
             return;
         }
 
-        m_currentCollider = other;
-        onTeleporterCollision.Raise();
-
         if (!otherTeleporterPosition)
         {
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
@@ -37,7 +70,29 @@ public class Teleporter : MonoBehaviour
             return;
         }
 
+        m_currentCollider = other;
+        onTeleporterCollision.Raise();
+
+        hasExitTeleporter.Value = false;
+        hasExitOtherTeleporter.Value = true;
+
         m_currentCollider.transform.position = otherTeleporterPosition.Value;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        Debug.Log(name + " on trigger exit");
+        if (!hasExitTeleporter)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            Debug.LogError("hasExitOtherTeleporter not set, can not check teleporter");
+#endif
+            return;
+        }
+
+        hasExitTeleporter.Value = true;
+        hasExitOtherTeleporter.Value = false;
     }
 
     public void OnOtherTeleporterCollision()
---
+++ Assets/Scripts/Variables/BoolVariable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RoboRyanTron.Unite2017.Variables
+{
+    [CreateAssetMenu]
+    public class BoolVariable : ScriptableObject
+    {
+        [SerializeField] private bool value = true;
+
+        public bool Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+}
