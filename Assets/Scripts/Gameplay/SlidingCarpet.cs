9599dadc8bd34e0b86cd9e95a380592c9968c92d
Moustafa Koterba <mou>
2019-08-03 15:05:03 +0200

    sliding carpet added

---
+++ Assets/Scripts/Gameplay/SlidingCarpet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingCarpet : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] private Vector3 slidingVector = Vector3.right;
+
+    private Collider m_currentCollider;
+    private bool m_isTriggered = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("on collision");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == null) return;
+        if (other.attachedRigidbody == null) return;
+
+        Debug.Log("on trigger");
+        m_isTriggered = true;
+        m_currentCollider = other;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("on trigger exit");
+        m_isTriggered = false;
+        m_currentCollider = null;
+    }
+
+    private void Update()
+    {
+        if (!m_isTriggered) return;
+        m_currentCollider.transform.position += slidingVector;
+    }
+}
