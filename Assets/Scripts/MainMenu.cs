8db89d5690981a67be2ce34aed4d68e16f84518d
guillaume-gomez <guillaume.gomez846@gmail.com>
2019-08-04 19:30:58 +0200

    add wall sound + song in credit and main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,12 +8,14 @@ public class MainMenu : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
+    SoundManager.PlayMusic("music_1");
     //Invoke("NextScene", 10f);
   }
 
   // Update is called once per frame
   void Update () {
      if(Input.GetButtonDown("Submit")) {
+       SoundManager.StopMusic();
        NextScene();
      }
   }
